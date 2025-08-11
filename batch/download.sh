5c63383a37bcced589bd950db76586d72c0f0551
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2025-08-11 11:09:13 +0900

    Update Docker image tags in workflow and download script to use 'examtopics' instead of 'examtopics-downloader'

--- batch/download.sh
+++ batch/download.sh
@@ -1,6 +1,6 @@
 docker run -it \
   --name examtopics-downloader \
-  ghcr.io/finbertmds/examtopics-downloader:latest \
+  ghcr.io/finbertmds/examtopics:latest \
   -p amazon -s aws-certified-machine-learning-engineer-associate-mla-c01 \
   -save-links -o output.md
 docker cp examtopics-downloader:/app/output.md ./md/aws_mla_c01.md
